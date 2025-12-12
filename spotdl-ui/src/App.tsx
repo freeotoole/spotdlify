2032f96e1cce2a74132aed4cbda702116ea360f2
Free O'Toole <14006733+freeotoole@users.noreply.github.com>
2025-12-13 06:51:58 +1100

    add styling and icon switching

--- spotdl-ui/src/App.tsx
+++ spotdl-ui/src/App.tsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useRef, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { useLogs } from "./hooks/useLogs";
+
+import { ReactComponent as SpotifyIcon } from "./assets/icons/spotify-brands-solid-full.svg";
+import { ReactComponent as PooIcon } from "./assets/icons/poo-solid-full.svg";
+import { ReactComponent as BombIcon } from "./assets/icons/bomb-solid-full.svg";
+import { ReactComponent as SkullIcon } from "./assets/icons/skull-solid-full.svg";
 
 function App() {
   const [url, setUrl] = useState("");
@@ -10,50 +14,26 @@ function App() {
   const [sessionId, setSessionId] = useState<string | null>(null);
 
   const containerRef = useRef<HTMLUListElement | null>(null);
-  const mockResponse = {
-    lines: [
-      {
-        stream: "stdout",
-        text: "Processing query:                                                               \nhttps://open.spotify.com/playlist/6fjOnyqUZ6foGNVY8Ye0yQ?si=6fb1eaeb256c4532    \n",
-      },
-      {
-        stream: "stdout",
-        text: "Found 5 songs in Dubstep (Playlist)                                             \n",
-      },
-      {
-        stream: "stdout",
-        text: "Skipping Excision - Existence - VIP (file already exists) (duplicate)           \n",
-      },
-      {
-        stream: "stdout",
-        text: "Skipping Excision - Not Enough (file already exists) (duplicate)                \n",
-      },
-      {
-        stream: "stdout",
-        text: "Skipping Excision - Blue Steel (file already exists) (duplicate)                \n",
-      },
-      {
-        stream: "stdout",
-        text: "Skipping Excision - 2005 (file already exists) (duplicate)                      \n",
-      },
-      {
-        stream: "stdout",
-        text: "Skipping Excision - Bonebreaker (file already exists) (duplicate)               \n",
-      },
-      {
-        stream: "stdout",
-        text: "\n",
-      },
-      {
-        stream: "meta",
-        text: 'Finished: {"success":true,"code":0,"url":"https://open.spotify.com/playlist/6fjOnyqUZ6foGNVY8Ye0yQ?si=6fb1eaeb256c4532"}',
-      },
-    ],
-    finished: true,
-    error: null,
+
+  // const { lines, finished, error } = mockResponse;
+  const { lines, finished, error } = useLogs(sessionId);
+
+  // create array of icons to randomly choose from
+  const icons = [
+    <PooIcon className="icon" />,
+    <BombIcon className="icon" />,
+    <SkullIcon className="icon" />,
+  ];
+
+  const iconColours = ["#53410e", "black", "white"];
+  const switchIcon = () => {
+    // change to a random icon index
+    let newIndex = Math.floor(Math.random() * icons.length);
+    while (newIndex === iconIndex) {
+      newIndex = Math.floor(Math.random() * icons.length);
+    }
+    setIconIndex(newIndex);
   };
-  const { lines, finished, error } = mockResponse;
-  // const { lines, finished, error } = useLogs(sessionId);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -74,7 +54,7 @@ function App() {
       console.log("[App] download response", res.status, data);
 
       if (res.status === 202 && data.id) {
-        setStatus("Streaming logs...");
+        // setStatus("🏴‍☠️ Hacking the narr...");
         setSessionId(data.id);
         // also log and surface the id so we can debug
         console.log("[App] got session id", data.id);
@@ -82,12 +62,15 @@ function App() {
         setStatus(`Error: ${data.error || data.status}`);
       } else {
         // fallback: older backend responses
+        console.log("Status: Download finished");
         setStatus("Download finished");
       }
     } catch (err: any) {
       setStatus("Request failed");
     } finally {
-      setLoading(false);
+      // setLoading(false);
+      console.log("Status: All done yo");
+      switchIcon();
     }
   };
 
@@ -96,52 +79,82 @@ function App() {
   // }, [lines, finished, error]);
 
   // auto-scroll to bottom when new logs arrive
-  useEffect(() => {
-    const el = containerRef.current;
-    if (el) {
-      el.scrollTo({ top: el.scrollHeight, behavior: "smooth" });
-    }
-  }, [lines]);
+  // useEffect(() => {
+  //   const el = containerRef.current;
+  //   if (el) {
+  //     el.scrollTo({ top: el.scrollHeight, behavior: "smooth" });
+  //   }
+  // }, [lines]);
+
+  const [iconIndex, setIconIndex] = useState(0);
+
+  const bbMessages = {
+    loading: "Processing your request...",
+    finished: "All done! Check your downloads folder.",
+    error: "An error occurred. Please try again.",
+  };
 
   return (
     <div className="App">
-      <header className="container-">
+      <div className="container flex column gap-xl">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>Paste link to Spotify song/album/playlist to download in MP3</p>
+        <h1>Paste link to Spotify song/album/playlist to download in MP3</h1>
 
-        <form className="ripper" onSubmit={handleSubmit}>
+        <form className="form-backdrop" onSubmit={handleSubmit}>
           <textarea
-            className="spotify-url"
             value={url}
             onChange={(e) => setUrl(e.target.value)}
             placeholder="Paste your Spotify URL here my dude..."
             rows={3}
           />
-          <button type="submit" disabled={loading}>
-            {loading ? "Ripping..." : "Rip it!"}
-          </button>
+          <p className="button-wrapper">
+            <button
+              type="submit"
+              disabled={loading}
+              className={loading ? "gay" : ""}
+            >
+              <span className="button-text">
+                {loading ? "Ripping..." : "RIP IT!"}
+              </span>
+              <span
+                className="button-icon"
+                style={{
+                  color: loading
+                    ? iconColours[iconIndex]
+                    : "var(--color-spotify)",
+                }}
+              >
+                {!loading ? (
+                  <SpotifyIcon
+                    className="icon"
+                    style={{
+                      fill: "var(--color-spotify)",
+                    }}
+                  />
+                ) : (
+                  icons[iconIndex]
+                )}
+              </span>
+            </button>
+          </p>
         </form>
-
-        {/* {status && <p className="status">{status}</p>} */}
-      </header>
-      {sessionId ||
-        (mockResponse && (
-          <section className="logs-section">
-            <div className="container-xl">
-              <ul className="logs" ref={containerRef} style={{}}>
-                {lines
-                  .filter((l) => l.stream === "stdout" && l.text.trim())
-                  .map((l, i) => (
-                    <li key={i}>{l.text}</li>
-                  ))}
-              </ul>
-              {error && <div style={{ color: "tomato" }}>{error}</div>}
-              {finished && (
-                <div style={{ color: "#a3f7b5" }}>Process finished</div>
-              )}
-            </div>
-          </section>
-        ))}
+        {/* TODO: add the ripping, finished etc messaging */}
+      </div>
+      <section className="logs-section">
+        <div className="container-xl">
+          {loading && <p>{bbMessages.loading}</p>}
+          {sessionId && (
+            <ul className="logs" ref={containerRef} style={{}}>
+              {lines
+                .filter((l) => l.stream === "stdout" && l.text.trim())
+                .map((l, i) => (
+                  <li key={i}>{l.text}</li>
+                ))}
+            </ul>
+          )}
+          {error && <div style={{ color: "tomato" }}>{error}</div>}
+        </div>
+      </section>
     </div>
   );
 }
